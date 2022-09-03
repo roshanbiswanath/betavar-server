5e81e242d3c88cb036bd9dda68bda8c571b55354
P.Biswanath Patra <63574390+roshanbiswanath@users.noreply.github.com>
2022-09-03 15:47:19 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile('../client/index.html');
+})
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+})
+
+http.listen(3000, function () {
+    console.log('Server is running on port 3000');
+})
+/*
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});*/
